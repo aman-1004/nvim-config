3ddb3031b286b3f6ff59abd91cee8ab03d5817e9
aman-1004 <2020ceb1004@iitrpr.ac.in>
2023-11-06 08:38:13 +0530

    ><

--- lua/Config/config.lua
+++ lua/Config/config.lua
@@ -8,6 +8,7 @@ o.softtabstop = tabSize
 o.relativenumber = true
 o.expandtab = true
 o.ignorecase = true
+o.wrapscan = false;
 -- o.scrolloff = 8
 -- vim.api.nvim_command('filetype plugin indent on')
 
